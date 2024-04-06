8c6aa1491c393d5d0e61ef7c2c43da79047a47c7
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-04-06 15:50:36 +0530

    done csv data work

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Cards from "../src/modules/csv-reader";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center">CSV FILE DATA</h1>
+      <div className="main-div">
+        <Cards />
+      </div>
+        <br/>
+    </main>
+  );
+}
---
+++ src/modules/csv-reader.tsx
@@ -0,0 +1,99 @@
+"use client"
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+
+interface CSVData {
+    Category: string;
+    Emotion: string;
+    Idea: string;
+}
+
+const Cards = () => {
+    const [filedata, setFileData] = useState<CSVData[]>([]);
+    const [displayedCards, setDisplayedCards] = useState<CSVData[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [isFlipped, setIsFlipped] = useState<boolean>(false); // State variable to toggle flip animation
+    const [isSearchVisible, setIsSearchVisible] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('./data/short-ideas.csv');
+                const text = await response.text();
+                const parsedData = Papa.parse<CSVData>(text, { header: true });
+                const data = parsedData.data;
+                setFileData(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        let filteredData = filedata;
+        if (searchTerm !== '') {
+            filteredData = filedata.filter(item => item.Emotion.toLowerCase().includes(searchTerm.toLowerCase()));
+        }
+        setDisplayedCards(filteredData.slice(0, 9)); // Display the first 9 cards
+        setIsFlipped(true); // Trigger flip animation
+        setTimeout(() => {
+            setIsFlipped(false); // Reset flip animation after a short delay
+        }, 500);
+    }, [filedata, searchTerm]);
+
+    useEffect(() => {
+        if (searchTerm === '') {
+            const intervalId = setInterval(() => {
+                const startIndex = Math.floor(Math.random() * (filedata.length - 9));
+                const displayedSet = filedata.slice(startIndex, startIndex + 9);
+                setDisplayedCards(displayedSet);
+                setIsFlipped(true); // Trigger flip animation
+                setTimeout(() => {
+                    setIsFlipped(false); // Reset flip animation after a short delay
+                }, 500);
+            }, 5000);
+
+            return () => clearInterval(intervalId);
+        }
+    }, [filedata, searchTerm]);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+    const toggleSearchVisibility = () => {
+        setIsSearchVisible(!isSearchVisible);
+    };
+
+    const clearSearch = () => {
+        setSearchTerm('');
+    };
+
+    return (
+        <>
+             <div className="search-icon bottom-10 right-10 absolute z-50" onClick={toggleSearchVisibility}>
+                {isSearchVisible ? <FaTimes onClick={clearSearch} /> : <FaSearch />}
+            </div>
+            {isSearchVisible && (
+                <div className="search-input">
+                    <input
+                        type="text"
+                        placeholder="Search by Emotion"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        className="search_bar"
+                    />
+                </div>
+            )}
+            <div className="card-container">
+                {displayedCards.map((item, i) => (
+                    <div className={`box ${isFlipped ? 'flip' : ''}`} key={i}>{item.Idea}</div>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default Cards;
